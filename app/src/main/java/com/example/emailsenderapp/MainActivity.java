1cb91eef0c2bfb0f9ac84578d6668a985a0a096d
Ruthika <ruthirockstar2218@gmail.com>
2024-05-13 11:09:43 +0530

    works ook

---
+++ app/src/main/java/com/example/emailsenderapp/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.emailsenderapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextAddress; private
+    EditText editTextSubject; private
+    EditText editTextMessage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextAddress = findViewById(R.id.edit_text_address);
+        editTextSubject = findViewById(R.id.edit_text_subject);
+        editTextMessage = findViewById(R.id.edit_text_message);
+        Button sendEmailButton = findViewById(R.id.send_email_button);
+        sendEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendEmail();
+            }
+        });
+    }
+    private void sendEmail() {
+        String[] recipientList = {editTextAddress.getText().toString()}; String
+                subject = editTextSubject.getText().toString();
+        String message = editTextMessage.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, recipientList);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(Intent.createChooser(intent, "Choose an Email client"));
+        }
+    }
+}
